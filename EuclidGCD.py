3cfdd1930c6eb8fd3312f6704b5395394d815f7c
Devin Simoneaux <devin.simoneaux@myemail.indwes.edu>
2015-12-02 19:26:21 +0000

    Week Last Assignment

--- EuclidGCD.py
+++ EuclidGCD.py
@@ -1,4 +1,13 @@
 def gCd(x, y):
-    if ???# base case here
-        return ???
-    return gCd(?????)
+    if y==0:
+        return x
+    else:
+        return gCd(y, x%y)
+    
+def main():
+    x=eval(input("Pick a number."))
+    y=eval(input("Pick another number."))
+    print("GCD of",x,",",y,"=",gCd(x,y))
+
+if __name__ == "__main__":
+    main()
